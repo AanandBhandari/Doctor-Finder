048d0b9904421d5b0af59beeb701e16fc956e9fc
AanandBhandari <aanandbhandari143@gmail.com>
2019-06-08 22:55:32 +0545

    basic client setup with react

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const cors = require('cors')
 // database connection
 mongoose
     .connect(process.env.MONGO_URI, { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false })
@@ -12,6 +13,9 @@ mongoose
 // express bodyparser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// for cross origin resourse sharing
+app.use(cors());
 
 // api routes middleware
 app.use("/api",require('./routes/doctor/auth'));
